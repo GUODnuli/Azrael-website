36b036bd2d40db47258d494a5735f45d0b1e2ecf
zhuangxiaosen <624115642@qq.com>
2024-05-12 15:48:28 +0800

    a little undate.

--- src/components/footer.rs
+++ src/components/footer.rs
@@ -72,7 +72,7 @@ pub fn GoBack(content: String) -> impl IntoView {
             <div class="text-center">
                 <a
                     class="text-base w-full sm:w-auto inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold text-blue-500 hover:text-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all  px-4 ring-offset-slate-900"
-                    href="../"
+                    href="/"
                 >
                     <svg class="w-2.5 h-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none">
                         <path
